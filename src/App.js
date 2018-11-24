2de51bff4b8682b9556451cf6a85daa54a8650dd
Sail338 <srihari.shankar1@gmail.com>
2018-11-24 18:47:33 -0500

    fix bugs with login and add form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Login from './Login'
 
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         Welcome To VHX Scheduler
         Login below
+        <Login />
       </div>
     );
   }
--- src/Login.jsx
+++ src/Login.jsx
@@ -3,15 +3,31 @@ import React, { Component } from 'react';
 
 export default class Login extends Component {
     constructor(){
+        super()
         this.state = {
-            login = "",
-            password = ""
+            login:"",
+            password:""
         }
 }
+ handleSubmit(){
+    console.log("fuck")
+ }
   render() {
     return (
       <div className="LoginForm">
-        
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input type="text" />
+        </label>
+    
+
+          <label>
+          password:
+          <input type="password"/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       </div>
     );
   }
