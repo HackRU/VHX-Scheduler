f3ab73dbfea31cd8b3025b9e27c4265f0b6a88dc
Sail338 <srihari.shankar1@gmail.com>
2018-11-24 18:19:33 -0500

    inital commit

---
+++ src/Login.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default class Login extends Component {
+    constructor(){
+        this.state = {
+            login = "",
+            password = ""
+        }
+}
+  render() {
+    return (
+      <div className="LoginForm">
+        
+      </div>
+    );
+  }
+}
