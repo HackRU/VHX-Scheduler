89c565c74bd754d91d5b160964cfac1a786b02ff
Sail338 <srihari.shankar1@gmail.com>
2018-11-24 18:59:18 -0500

    login componet has state now

--- src/Login.jsx
+++ src/Login.jsx
@@ -8,23 +8,30 @@ export default class Login extends Component {
             login:"",
             password:""
         }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 }
- handleSubmit(){
-    console.log("fuck")
- }
+handleChange = event => {
+    console.log(event.target)
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
   render() {
     return (
       <div className="LoginForm">
         <form onSubmit={this.handleSubmit}>
         <label>
           Username:
-          <input type="text" />
+          <input type="text" id='login' value={this.state.login} onChange={this.handleChange}/>
         </label>
-    
-
           <label>
           password:
-          <input type="password"/>
+          <input type="password" id='password'  value={this.state.password} onChange={this.handleChange}/>
         </label>
         <input type="submit" value="Submit" />
       </form>
