bfa4485c997635e09612ee236b9cd1d7470a7521
Sail338 <srihari.shankar1@gmail.com>
2019-01-10 15:48:47 -0500

    add toggle function

--- src/MagicLink.jsx
+++ src/MagicLink.jsx
@@ -7,9 +7,9 @@ export default class MagicLinks extends Component {
         this.state = {
             emailToList: "",
             isVolunteer: false,
-            isDirector:false,
-            isOrganizer:false,
-            isJudge:false
+            isDirector: false,
+            isOrganizer: false,
+            isJudge: false
         }
     }
     handleChange = event => {
@@ -17,20 +17,80 @@ export default class MagicLinks extends Component {
             [event.target.id]: event.target.value
         });
     }
-    handleSubmit = event =>{
+
+    toggleChangeVolunteer = () => {
+        this.setState(prevState => ({
+          isVolunteer: !prevState.isVolunteer,
+        }));
+      }
+
+      toggleChangeDirector = () => {
+        this.setState(prevState => ({
+          isDirector: !prevState.isDirector,
+        }));
+      } 
+
+      toggleChangeJudge = () => {
+        this.setState(prevState => ({
+          isJudge: !prevState.isJudge,
+        }));
+      } 
+
+      toggleChangeOrganizer = () => {
+        this.setState(prevState => ({
+          isOrganizer: !prevState.isOrganizer,
+        }));
+      } 
+    handleSubmit = event => {
         event.preventDefault();
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                 <form onSubmit={this.handleSubmit}>
-                 <label>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
                         Enter the emails(s) you want to send magic links to (Comma Seperated if multiple)
                         <input type="text" id='emailToList'
                             value={this.state.emailToList} onChange={this.handleChange} />
-                </label>
+                    </label>
+
+                    <label className="form-check-label">
+                        <input type="checkbox"
+                            checked={this.state.isVolunteer}
+                            onChange={this.toggleChangeVolunteer}
+                            className="form-check-input"
+                        />
+                        Volunteer
+                    </label>
+
+                    <label className="form-check-label">
+                        <input type="checkbox"
+                            checked={this.state.isDirector}
+                            onChange={this.toggleChangeDirector}
+                            className="form-check-input"
+                        />
+                        Director
+                    </label>
+
+                    <label className="form-check-label">
+                        <input type="checkbox"
+                            checked={this.state.isOrganizer}
+                            onChange={this.toggleChangeOrganizer}
+                            className="form-check-input"
+                        />
+                        Organizer
+                    </label>
+
+                    <label className="form-check-label">
+                        <input type="checkbox"
+                            checked={this.state.isJudge}
+                            onChange={this.toggleChangeJudge}
+                            className="form-check-input"
+                        />
+                        Judge
+                    </label>
                     <input type="submit" value="Submit" />
-                 </form>
+                </form>
             </div>
         )
     }
